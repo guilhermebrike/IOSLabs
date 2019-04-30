6587bec8655c77cb99a3ab59426b5f98b9307d37
Guilherme <guilhermebrike@gmail.com>
2019-04-30 16:24:52 -0700

    finished stretch goals

--- TipCalculator/TipCalculator/Controllers/CalculatorViewController.swift
+++ TipCalculator/TipCalculator/Controllers/CalculatorViewController.swift
@@ -14,19 +14,38 @@ class CalculatorViewController: UIViewController {
     let billAmountTextField: UITextField = {
         let tf = UITextField()
         tf.backgroundColor = .blue
-        tf.widthAnchor.constraint(equalToConstant: 64).isActive = true
-        tf.heightAnchor.constraint(equalToConstant: 64).isActive = true
-        tf.text = "bill"
+        tf.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        tf.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        tf.text = "0"
+        tf.addTarget(self, action: #selector(anyValueChanged(_:)), for: .editingChanged)
         return tf
     }()
     
     let percentageAmountTextField: UITextField = {
         let tf = UITextField()
         tf.backgroundColor = .blue
-        tf.widthAnchor.constraint(equalToConstant: 64).isActive = true
-        tf.heightAnchor.constraint(equalToConstant: 64).isActive = true
-        tf.text = "%"
+        tf.widthAnchor.constraint(equalToConstant: 30).isActive = true
+        tf.heightAnchor.constraint(equalToConstant: 20).isActive = true
+        tf.text = "0"
+        tf.addTarget(self, action: #selector(anyValueChanged(_:)), for: .editingChanged)
         return tf
+    }()
+    
+    
+    let percentageAmountSlider: UISlider = {
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.minimumValue = 0
+        slider.maximumValue = 100
+        slider.minimumTrackTintColor = .green
+        slider.maximumTrackTintColor = .cyan
+        slider.thumbTintColor = .black
+        slider.isContinuous = true
+        slider.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        slider.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        slider.addTarget(self, action: #selector(changeValueSlider(_:)), for: .valueChanged)
+        slider.addTarget(self, action: #selector(anyValueChanged(_:)), for: .valueChanged)
+        return slider
     }()
     
     let calculateTipButton: UIButton = {
@@ -57,7 +76,6 @@ class CalculatorViewController: UIViewController {
         return sv
     }()
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
@@ -66,6 +84,8 @@ class CalculatorViewController: UIViewController {
         myAppStackView.addArrangedSubview(billAmountTextField)
         
         myAppStackView.addArrangedSubview(percentageAmountTextField)
+        
+        myAppStackView.addArrangedSubview(percentageAmountSlider)
         
         myAppStackView.addArrangedSubview(calculateTipButton)
         
@@ -76,8 +96,30 @@ class CalculatorViewController: UIViewController {
             myAppStackView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             myAppStackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             ])
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+        
+        self.hideKeyboardWhenTappedAround()
     }
     
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            print("notification: Keyboard will show")
+            if self.view.frame.origin.y == 0{
+                self.view.frame.origin.y -= keyboardSize.height / 2
+            }
+        }
+        
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        if ((notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue) != nil {
+                self.view.frame.origin.y = 0
+        }
+    }
     
     @objc private func calculateTip() {
         let bill = Double(billAmountTextField.text!)!
@@ -86,5 +128,27 @@ class CalculatorViewController: UIViewController {
     }
     
     
+    @objc func changeValueSlider(_ sender: UISlider) {
+        percentageAmountTextField.text = String(Int(sender.value))
+    }
+    
+    @objc func anyValueChanged(_ sender: UISlider) {
+        guard let bill = Double(billAmountTextField.text ?? "0") else { return }
+        guard let percentage = Double(percentageAmountTextField.text ?? "0") else { return }
+        tipAmountLabel.text = "\(bill * percentage / 100)"
+    }
+}
 
+
+extension UIViewController {
+    
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
