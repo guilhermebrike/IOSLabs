aba0786c6827c2eb607a32f3fa4cdc142b278b0f
guilhermebrike <guilhermebrike@gmail.com>
2019-05-17 13:12:57 -0700

    finished todo

---
+++ PersonalityQuiz/PersonalityQuiz/IntroductionViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Guilherme Wahlbrink on 2019-05-16.
+//  Copyright © 2019 Guilherme Wahlbrink. All rights reserved.
+//
+
+import UIKit
+
+class IntroductionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PersonalityQuiz/PersonalityQuiz/ResultsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ResultsViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Guilherme Wahlbrink on 2019-05-16.
+//  Copyright © 2019 Guilherme Wahlbrink. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
